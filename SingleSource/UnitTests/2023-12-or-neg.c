8797f5b9ca53e35bf72cc97e830fe5615a411275
thealmarty <thealmartyblog@gmail.com>
2023-12-11 15:27:14 -0800

    Add and, or, xor tests.

--- SingleSource/UnitTests/2023-12-or-neg.c
+++ SingleSource/UnitTests/2023-12-or-neg.c
@@ -4,7 +4,7 @@ int main()
     int v2 = 9;
     int v3 = v1 | v2;
     // 1100 | 1001 = 1101
-    if (v3 == 13) {
+    if (v3 == 3) {
         while (1){}
     }
 
--- SingleSource/UnitTests/2023-12-xor-neg.c
+++ SingleSource/UnitTests/2023-12-xor-neg.c
@@ -4,7 +4,7 @@ int main()
     int v2 = 12;
     int v3 = v1 ^ v2;
     // 1001 ^ 1100 = 0101
-    if (v3 == 5) {
+    if (v3 == 15) {
         while (1){}
     }
 
