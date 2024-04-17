a06a3f51641d408b0ccede012cf87eb25fd5aa4e
Alan Li <alan.li@me.com>
2024-04-16 22:08:57 -0400

    implement indirect branch c test

---
+++ SingleSource/UnitTests/2024-indirect-branch.c
@@ -0,0 +1,13 @@
+__attribute__((noinline))
+unsigned add(unsigned a, unsigned b) {
+    return a + b;
+}
+
+int main() {
+    // Declare a function pointer and initialize it to point to the add function
+    unsigned (*funcPtr)(unsigned, unsigned) = add;
+    // Now, call the add function indirectly using the function pointer
+    unsigned result = funcPtr(5, 3);
+    return result;
+}
+
