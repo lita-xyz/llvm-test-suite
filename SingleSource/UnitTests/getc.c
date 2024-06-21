ef4a6f591eaccfebf500ae20891938ea1253c14b
thealmarty <thealmartyblog@gmail.com>
2024-06-21 08:35:29 -0700

    Add getc putc tests.

---
+++ SingleSource/UnitTests/getc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+  char c;
+
+  c = getc(stdin);
+  putc(c, stdout);
+   
+  return(0);
+}
---
+++ SingleSource/UnitTests/putc.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+putc( 'c', stdout ) ;
+
+  return 0;
+}
